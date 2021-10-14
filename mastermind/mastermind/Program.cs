458a92ceb40b63a09474e9e87e7718059e61c3f6
gregorymartin <77732347+gregorymartin200261@users.noreply.github.com>
2021-10-14 16:05:10 +0200

    ajout du c# pour le choix aléatoire des couleurs

---
+++ mastermind/mastermind/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] combi;
+            ChoixCombi(out combi); //out car sortie
+            Afficher(combi);       //pas de out car affichage
+        }
+
+        static void ChoixCombi(out int [] combi){
+            combi = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                Random rnd = new Random();
+                int valeur = rnd.Next(1, 7);
+                combi[i] = valeur;
+            }
+        }
+
+        static void Afficher(int[] combi)
+        {
+            for (int i = 0; i <= 3; i++)
+            Console.Write(combi[i]);
+        }
+    }
+}
